0a2459c57471f96e640f3113fa75a11230a650d9
Al17ag <agomer225@gmail.com>
2024-09-16 23:01:03 +0200

    initial project structure

---
+++ pip.py
@@ -0,0 +1,10 @@
+# 1 .venv\Scripts\activate
+# pip install flask
+# pip install python-dotenv -связывает файл env с файлом init
+# pip install falsk_sql_alchemy
+# pip install flask-migrate
+
+# flask db init  перед запуском app
+# flask db migrate
+# flask db migrate -m 'initial db'
+# flask db upgrade
